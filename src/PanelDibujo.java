9d568ca008fc49857e619d6681a5928c30df1da1
DennisCatana <dennis.joel235@outlook.es>
2023-06-20 09:46:48 -0500

    Drwa

--- src/PanelDibujo.java
+++ src/PanelDibujo.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class PanelDibujo {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class PanelDibujo extends JPanel {
+    public void paintComponent(Graphics g){
+        System.out.print("Ingrese su peso en kg: ");
+        Scanner ingreso = new Scanner(System.in);
+        float dato;
+        dato = ingreso.nextFloat();
+
+        System.out.print("Ingrese su estatura en m: ");
+        Scanner ingreso2 = new Scanner(System.in);
+        float dato2;
+        dato2 = ingreso.nextFloat();
+
+        float IMC;
+        IMC=dato/(dato2*2);
+        String IMC2 = Float.toString(IMC);
+
+        System.out.println("Su IMC es: "+IMC);
+
+
+
+        super.paintComponent (g);
+        int anchura=getWidth();
+        int altura=getHeight();
+
+        /*g.drawLine(10,10,100,10);
+        g.drawLine(30,100,30,10);
+        g.drawLine(100,100,100,10);
+        g.drawLine(100,100,100,10);
+        g.drawLine(10,100,100,100);*/
+
+        g.drawString(IMC2,100,100);
+
+
+
+    }
 }
--- src/PruebaPanelDibujo.java
+++ src/PruebaPanelDibujo.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class PruebaPanelDibujo {
+import javax.swing.*;
+
+
+public class PruebaPanelDibujo {
+    public static void main(String[] args) {
+
+        //Creacion de una instancia de la clase PanelDbujo y la llamo panel
+        PanelDibujo panel = new PanelDibujo(); //Esto es una instancia
+        //Creo un marco en la ventana
+        JFrame aplicacion = new JFrame();
+        //Pongo un boton para cerrar la ventana
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        aplicacion.add(panel);//Agrego un "panel" a la vventana
+        aplicacion.setSize(250,250);//}seteo el tamaño de la ventana
+        aplicacion.setVisible(true);//Permito que se vea la ventana
+
+
+    }
 }
